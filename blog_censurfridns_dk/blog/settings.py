13fcf604e3014cc448b0a9adbc2c1e2355b95fa7
Thomas Steen Rasmussen <thomas@gibfest.dk>
2016-03-21 01:04:45 +0100

    add missing STATIC_ROOT

--- blog_censurfridns_dk/blog/settings.py
+++ blog_censurfridns_dk/blog/settings.py
@@ -74,6 +74,7 @@ LANGUAGE_DOMAINS = {
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # logging
 LOGGING = {
