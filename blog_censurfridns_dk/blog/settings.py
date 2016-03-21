014f6379167fa8fd53fd50493bcf944d4ce88e31
Thomas Steen Rasmussen <thomas@gibfest.dk>
2016-03-21 01:12:04 +0100

    remove staticfiles_dirs from settings

--- blog_censurfridns_dk/blog/settings.py
+++ blog_censurfridns_dk/blog/settings.py
@@ -73,7 +73,6 @@ LANGUAGE_DOMAINS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # logging
