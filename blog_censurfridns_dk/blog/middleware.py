d1980923ae31864f52c788f0e16b66e0dd42220c
Thomas Steen Rasmussen <thomas@gibfest.dk>
2016-03-25 16:15:46 +0100

    missing import, quote key

--- blog_censurfridns_dk/blog/middleware.py
+++ blog_censurfridns_dk/blog/middleware.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.utils import translation
 
 class SetLanguageFromDomainMiddleware(object):
     def process_request(self, request):
@@ -7,5 +8,5 @@ class SetLanguageFromDomainMiddleware(object):
             if request.META['HTTP_HOST'] in settings.LANGUAGE_BY_DOMAIN:
                 user_language = settings.LANGUAGE_BY_DOMAIN[request.META['HTTP_HOST']]
                 translation.activate(user_language)
-                request.session[LANGUAGE_SESSION_KEY] = user_language
+                request.session['LANGUAGE_SESSION_KEY'] = user_language
 
