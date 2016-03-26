0038425a000a0521b46a7a7f140bf539cd8909d8
Thomas Steen Rasmussen <thomas@gibfest.dk>
2016-03-26 19:08:50 +0100

    change created_date and modified_date to be regular datetimefields so they are editable in django admin

--- blog_censurfridns_dk/blog/models.py
+++ blog_censurfridns_dk/blog/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.utils.text import slugify
+from django.utils import timezone
 from django.core.urlresolvers import reverse
 from django.conf import settings
 from taggit.managers import TaggableManager
 
 
 class BlogPost(models.Model):
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(null=True, blank=True)
+    modified_date = models.DateTimeField(null=True, blank=True)
     title = models.CharField(max_length=100)
     body = models.TextField()
     slug = models.SlugField(blank=True, max_length=100)
@@ -24,12 +25,18 @@ class BlogPost(models.Model):
 
     def save(self, *args, **kwargs):
         if self.pk is None:
-            ### save to make sure we have an id for the slug
+            ### save to make sure we have an id (needed for the slug)
             super(BlogPost, self).save(*args, **kwargs)
+            if not self.created_date:
+                ### no created_date was specified in the form, set it to now()
+                self.created_date = timezone.now()
 
         ### set/update slugs for all enabled languages
         for code, language in settings.LANGUAGES:
             setattr(self, 'slug_%s' % code, '%s-%s' % (self.id, slugify(getattr(self, 'title_%s' % code))))
+
+        ### update modified_date
+        self.modified_date = timezone.now()
 
         ### save
         super(BlogPost, self).save(*args, **kwargs)
