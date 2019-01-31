be91c488266e327b6d6ede3e0263471182d2600b
Thomas Steen Rasmussen <admin@censurfridns.dk>
2019-01-31 07:59:31 +0100

    fix middleware to match the new post 1.10 middleware style

--- src/blog/middleware.py
+++ src/blog/middleware.py
@@ -1,17 +1,15 @@
 from django.conf import settings
 from django.utils import translation
 
-class SetLanguageFromDomainMiddleware(object):
+class SetLanguageFromDomainMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        return self.get_response(request)
-
-    def process_request(self, request):
         ### set language from domain
         if request.META['HTTP_HOST'] in settings.LANGUAGE_BY_DOMAIN:
             user_language = settings.LANGUAGE_BY_DOMAIN[request.META['HTTP_HOST']]
             translation.activate(user_language)
             request.session[translation.LANGUAGE_SESSION_KEY] = user_language
+        return self.get_response(request)
 
